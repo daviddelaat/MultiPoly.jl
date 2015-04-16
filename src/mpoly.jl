df4cc76d53528a82fa8cce0e6b97e1b260752c8c
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2015-04-16 10:07:35 +0200

    Changed to OrderedDict

--- src/mpoly.jl
+++ src/mpoly.jl
@@ -1,5 +1,5 @@
 immutable MPoly{T}
-    terms::Dict{Vector{Int},T}
+    terms::OrderedDict{Vector{Int},T}
     vars::Vector{Symbol}
 end
 
@@ -11,19 +11,19 @@ vars(p::MPoly) = p.vars
 
 nvars(p::MPoly) = length(vars(p))
 
-zero{T}(::Type{MPoly{T}}; vars::Vector{Symbol}=Symbol[]) = MPoly{T}(Dict{Vector{Int},T}(), vars)
+zero{T}(::Type{MPoly{T}}; vars::Vector{Symbol}=Symbol[]) = MPoly{T}(OrderedDict{Vector{Int},T}(), vars)
 zero{T}(p::MPoly{T}) = zero(MPoly{T}, vars=vars(p))
 
-one{T}(::Type{MPoly{T}}; vars::Vector{Symbol}=Symbol[]) = MPoly{T}([zeros(Int, length(vars)) => one(T)], vars)
+one{T}(::Type{MPoly{T}}; vars::Vector{Symbol}=Symbol[]) = MPoly{T}(OrderedDict(zeros(Int, length(vars)) => one(T)), vars)
 one{T}(p::MPoly{T}) = one(MPoly{T}, vars=vars(p))
 
 function generators{T}(::Type{MPoly{T}}, vars::Symbol...)
      m = eye(Int, length(vars))
-     [MPoly{T}([m[:,i] => one(T)], [vars...]) for i = 1:length(vars)]
+     [MPoly{T}(OrderedDict(m[:,i] => one(T)), [vars...]) for i = 1:length(vars)]
 end
 
 generator{T}(::Type{MPoly{T}}, var::Symbol) =
-     MPoly{T}([[1] => one(T)], [var])
+     MPoly{T}(OrderedDict([1] => one(T)), [var])
 
 promote_rule{T,U}(::Type{MPoly{T}}, ::Type{MPoly{U}}) = MPoly{promote_type(T, U)}
 promote_rule{T,U}(::Type{MPoly{T}}, ::Type{U}) = MPoly{promote_type(T, U)}
@@ -36,12 +36,12 @@ function convert{T}(P::Type{MPoly{T}}, p::MPoly)
     r
 end
 
-convert{T}(::Type{MPoly{T}}, c::T) = 
-    MPoly{T}([Int[] => c]) 
+convert{T}(::Type{MPoly{T}}, c::T) =
+    MPoly{T}(OrderedDict(Int[] => c))
 
 monomials(p::MPoly) = keys(terms(p))
 
-getindex{T}(p::MPoly{T}, m::Vector{Int}) = 
+getindex{T}(p::MPoly{T}, m::Vector{Int}) =
     get(terms(p), m, zero(T))
 
 getindex(p::MPoly, m::Int...) = p[[m...]]
@@ -56,7 +56,7 @@ function setindex!{T}(p::MPoly{T}, v, m::Int...)
 end
 
 setindex!{T}(p::MPoly{T}, v, m::Vector{Int}) = p[m...] = v
-    
+
 start(p::MPoly) = start(terms(p))
 next(p::MPoly, state) = next(terms(p), state)
 done(p::MPoly, state) = done(terms(p), state)
